2b48630457c1830388f1a7ccae051862963b7586
Alina Ray <a.alina.ray@gmail.com>
2019-08-18 20:15:51 +0300

    Updates

---
+++ precache-manifest.6cdeef7910f687b0d5421f3b137980e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e384464f583f93ef02e6e898a2ccc7",
+    "url": "/todos-app/index.html"
+  },
+  {
+    "revision": "d34c46ed98fbdd3105cb",
+    "url": "/todos-app/static/css/main.8ef62f50.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/todos-app/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "d34c46ed98fbdd3105cb",
+    "url": "/todos-app/static/js/main.f4feeadb.chunk.js"
+  },
+  {
+    "revision": "b1aaddfe4238e98cd73c",
+    "url": "/todos-app/static/js/runtime~main.985c0ea8.js"
+  }
+]);
